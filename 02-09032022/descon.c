f2196034cf94a68b489687cb1e605436e518d850
Gustavo de Lucas <gustavodelucas09@gmail.com>
2022-03-11 11:19:52 -0300

    Estudo da Linguagem C

---
+++ 02-09032022/descon.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+
+{
+    float preco, taxa, desconto, resultado;
+
+    printf("Digite um preço para o produto: ");
+    scanf("%f", &preco);
+
+    printf("Digite a taxa de desconto: ");
+    scanf("%f", &taxa);
+    /* O vendedor ira entrar com o valor de taxa e em seguida nosso programa divide por 100 para obter o valor em percentuual. sendo assim, quando o vendedor digitar 5, o programa vai calcular e teremos o resultado 0,05 que representa 5%*/
+
+    taxa = taxa / 100;
+
+    desconto = preco * taxa;
+
+    resultado = preco - desconto;
+
+    printf("O valor do desconto é %f e o valor é %f", desconto, resultado);
+    return 0;
+}
